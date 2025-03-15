2ea91e834106ff7d9179fe7a05eebf19df08996a
maxcoder12 <maxgblds@gmail.com>
2025-03-15 11:37:33 -0300

    need-js

---
+++ chamber/scripts/lastModified.js
@@ -0,0 +1,12 @@
+const today = new Date();
+
+const currentYear = document.querySelector("#currentYear")
+const lastModified = document.querySelector("#lastModified")
+
+
+const fullYear = today.getFullYear()
+
+const lastModification = document.lastModified
+
+currentYear.textContent = fullYear
+lastModified.textContent = `Last Modification: ${lastModification}`
