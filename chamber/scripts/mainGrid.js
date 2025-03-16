6b5d15e3b0123be4457a40c8804ea035c6200c92
maxcoder12 <maxgblds@gmail.com>
2025-03-16 01:30:39 -0300

    all-done

---
+++ chamber/scripts/mainGrid.js
@@ -0,0 +1,13 @@
+const gridBt = document.querySelector("#gridButton");
+const oneClBt = document.querySelector("#oneColumn");
+
+const memberCt = document.querySelector("#membersContainer");
+const memberCards = document.querySelectorAll("#membersContainer div");
+
+gridBt.addEventListener("click", () =>{
+    memberCt.style.flexDirection = "row";
+});
+
+oneClBt.addEventListener("click", () =>{
+    memberCt.style.flexDirection = "column";
+});
